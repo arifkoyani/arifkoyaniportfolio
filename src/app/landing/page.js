ff437cbb8c1dffc83e94537eb9f1324270dabff2
arifkoyani <arifkoyani@gmail.com>
2024-06-24 22:01:42 +0500

    add code

--- src/app/landing/page.js
+++ src/app/landing/page.js
@@ -1,24 +1,26 @@
 "use client";
-import Leftbox from "../Landing/LeftBox/LeftBox";
+import LeftBox from "../Landing/LeftBox/LeftBox";
 import RightBox from "../Landing/rightbox/page";
 import PointsBar from "../Landing/pointsbar/page";
-import Aboutme from "./aboutme/page";
-import Myskills from "./myskills/page";
+import AboutMe from "../Landing/aboutme/page";
+import MySkills from "./myskills/page";
 import ContactUS from "./contactUs/page";
 import Reviews from "./Reviews/page";
+
 const Landing = () => {
   return (
     <>
       <div className="w-[100%] bg-[#141c27] flex justify-around">
-        <Leftbox />
+        <LeftBox />
         <RightBox />
       </div>
       <PointsBar />
-      <Aboutme />
-      <Myskills />
+      <AboutMe />
+      <MySkills />
       <ContactUS />
       <Reviews />
     </>
   );
 };
+
 export default Landing;
