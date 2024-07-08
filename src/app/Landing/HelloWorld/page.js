6db6ebfa6312d055684bc657329ce1c0e79cc33e
arifkoyani <arifkoyani@gmail.com>
2024-07-08 05:19:11 +0500

    add new

--- src/app/Landing/HelloWorld/page.js
+++ src/app/Landing/HelloWorld/page.js
@@ -5,7 +5,7 @@ const HellWorld = () => {
     <>
       <div className="flex min-h-screen items-center justify-center bg-[#141c2789]">
         <div className="w-max">
-          <h1 className="animate-typing overflow-hidden whitespace-nowrap border-r-4 border-r-white pr-5 text-3xl text-white font-light">
+          <h1 className=" hellowordtext animate-typing overflow-hidden whitespace-nowrap border-r-4 border-r-white pr-5 text-3xl text-white font-light">
             I&apos;m Into Software Engineering..............!.
           </h1>
         </div>
